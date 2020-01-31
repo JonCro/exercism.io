85310569afc079b9a71e93967ec8da332cbf4bd2
Jon Cronin <jcronin42@gmail.com>
2020-01-31 12:12:49 -0500

    updated pangram solution

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,41 +1,7 @@
+// prettier-ignore
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 export const isPangram = sentence => {
-  const sentenceToLowerCase = sentence.toLowerCase();
-  const alphabet = {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0,
-    h: 0,
-    i: 0,
-    j: 0,
-    k: 0,
-    l: 0,
-    m: 0,
-    n: 0,
-    o: 0,
-    p: 0,
-    q: 0,
-    r: 0,
-    s: 0,
-    t: 0,
-    u: 0,
-    v: 0,
-    w: 0,
-    x: 0,
-    y: 0,
-    z: 0
-  };
-
-  for (const el of sentenceToLowerCase) {
-    for (const prop in alphabet) {
-      if (prop == el) {
-        alphabet[prop] = 1;
-      }
-    }
-  }
-
-  return Object.values(alphabet).includes(0) ? false : true;
+  sentence = sentence.toLowerCase();
+  return alphabet.every(el => sentence.includes(el));
 };
